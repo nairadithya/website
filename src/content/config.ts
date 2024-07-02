52a1810995f0426ae980f5d109993ab5da40b002
Adithya Nair <adithyanair121@gmail.com>
2024-07-02 16:21:13 +0530

    Started writing for essays

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,12 @@
-import { z, defineCollection } from 'astro:content';
-
+import { z, defineCollection } from 'astro:content'
 const essayCollection = defineCollection({
     schema: z.object({
         title: z.string(),
         description: z.string(),
         image: z.string(),
     imageAlt: z.string(),
-  date: z.date(),
-  }),       
+        date: z.date(),
+    }),
 })
 
 export const collections = {
