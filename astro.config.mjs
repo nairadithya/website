0cb404a6e2dce72a6b4ce0b4235ee0e8d667a35c
Adithya Nair <adithyanair121@gmail.com>
2024-10-13 22:19:35 +0530

    added rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 import mdx from "@astrojs/mdx";
-
+{
+  shikiConfig: {
+    theme: "catppuccin-mocha"
+  }
+}
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()]
