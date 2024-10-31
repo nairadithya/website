e2a311ad758ab834db6c112f49714dc360a1ce42
Adithya Nair <adithyanair121@gmail.com>
2024-10-31 19:21:40 +0530

    Added styling to RSS, hopefully it works?

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,19 +1,20 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
 
 export async function GET(context) {
-  const essays = (await getCollection('essays')).sort(
-    (a, b) => b.data.date.getTime() - a.data.date.getTime()
-  );
-  return rss({
-    title: 'Adithya Nair\'s Website',
-    description: 'The digital home of one Adithya Nair.',
-    site: context.site,
-    items: essays.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.date,
-      description: post.data.description,
-      link: `/essays/${post.slug}/`,
-    })),
-  });
+    const essays = (await getCollection('essays')).sort(
+        (a, b) => b.data.date.getTime() - a.data.date.getTime()
+    )
+    return rss({
+        style: '/rss/styles.xsl',
+        title: "Adithya Nair's Website",
+        description: 'The digital home of one Adithya Nair.',
+        site: context.site,
+        items: essays.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.date,
+            description: post.data.description,
+            link: `/essays/${post.slug}/`,
+        })),
+    })
 }
