e0b3ecac05b7003113defdddc459339abcfccaa9
Adithya Nair <adithyanair121@gmail.com>
2024-10-31 22:58:14 +0530

    added images to rss????

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
+import sanitizeHtml from 'sanitize-html'
+import MarkdownIt from 'markdown-it'
+const parser = new MarkdownIt()
 
 export async function GET(context) {
     const essays = (await getCollection('essays')).sort(
@@ -15,6 +18,10 @@ export async function GET(context) {
             pubDate: post.data.date,
             description: post.data.description,
             link: `/essays/${post.slug}/`,
+            content: sanitizeHtml(parser.render(post.body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+            }),
+            ...post.data,
         })),
     })
 }
