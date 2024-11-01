afacd566534ecd255a3deeb0164d4ceedf6e6747
Adithya Nair <adithyanair121@gmail.com>
2024-11-01 12:02:02 +0530

    Fixed journal/essay dichotomy

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,20 +5,22 @@ import MarkdownIt from 'markdown-it'
 const parser = new MarkdownIt()
 
 export async function GET(context) {
-    var essays = await getCollection('essays')
-    var journal = await getCollection('journal')
-    essays.sort((a, b) => b.data.date.getTime() - a.data.date.getTime())
-    journal.sort((a, b) => b.data.date.getTime() - a.data.date.getTime())
+    var posts = await getCollection('essays')
+    posts.push.apply(posts, await getCollection('journal'))
+    posts.sort((a, b) => b.data.date.getTime() - a.data.date.getTime())
     return rss({
         stylesheet: '/rss/styles.xsl',
         title: "Adithya Nair's Website",
         description: 'A place where I wonder out loud.',
         site: context.site,
-        items: essays.map((post) => ({
+        items: posts.map((post) => ({
             title: post.data.title,
             pubDate: post.data.date,
             description: post.data.description,
-            link: `/essays/${post.slug}/`,
+            link:
+                post.collection == 'essays'
+                    ? `/essays/${post.slug}/`
+                    : `/journal/${post.slug}`,
             content: sanitizeHtml(parser.render(post.body), {
                 allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
             }),
