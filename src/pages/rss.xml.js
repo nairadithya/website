9a540387f94767af3c1f95ccc10366c845434842
Adithya Nair <adithyanair121@gmail.com>
2024-11-01 11:23:16 +0530

    Added RSS styling and also added an RSS button.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,11 +5,11 @@ import MarkdownIt from 'markdown-it'
 const parser = new MarkdownIt()
 
 export async function GET(context) {
-    const essays = (await getCollection('essays')).sort(
-        (a, b) => b.data.date.getTime() - a.data.date.getTime()
-    )
+    var essays = await getCollection('essays')
+    essays.push.apply(essays, await getCollection('journal'))
+    essays.sort((a, b) => b.data.date.getTime() - a.data.date.getTime())
     return rss({
-        style: '/rss/styles.xsl',
+        stylesheet: '/rss/styles.xsl',
         title: "Adithya Nair's Website",
         description: 'The digital home of one Adithya Nair.',
         site: context.site,
@@ -21,7 +21,6 @@ export async function GET(context) {
             content: sanitizeHtml(parser.render(post.body), {
                 allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
             }),
-            ...post.data,
         })),
     })
 }
