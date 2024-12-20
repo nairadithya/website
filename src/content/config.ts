e4ec169c09c9f9716af12c1b395c62ace3b76051
Adithya Nair <adithyanair121@gmail.com>
2024-12-20 20:24:47 +0530

    Small changes to links

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,24 +1,33 @@
-
 import { z, defineCollection } from 'astro:content'
 const essayCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.date(),
-  }),
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        date: z.date(),
+    }),
 })
 
 const journalCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.date(),
-  }),
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        date: z.date(),
+    }),
+})
+
+const booksCollection = defineCollection({
+    type: 'content',
+    scheme: z.object({
+        title: z.string(),
+        review: z.string(),
+        cover: image(),
+    }),
 })
 
 export const collections = {
-  essays: essayCollection,
-  journal: journalCollection
+    essays: essayCollection,
+    journal: journalCollection,
+    books: booksCollection,
 }
