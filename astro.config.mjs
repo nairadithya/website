ed026111e04ddec218badaca46d6bf91e73602d8
Adithya Nair <adithyanair121@gmail.com>
2025-03-22 14:45:05 +0530

    Forgot one file, also migrated to vite tailwind plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,19 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import rehypeMathjax from 'rehype-mathjax';
-import remarkMath from 'remark-math';
-import mdx from "@astrojs/mdx";
+import { defineConfig } from 'astro/config'
+import rehypeMathjax from 'rehype-mathjax'
+import remarkMath from 'remark-math'
+import tailwindcss from '@tailwindcss/vite'
+import mdx from '@astrojs/mdx'
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
-  site: 'https://adithyanair.com',
-  markdown: {
-    remarkPlugins: [
-      'remark-gfm', 'remark-smartypants',
-      'remark-math'
-    ],
-    rehypePlugins: [
-      'rehype-mathjax'
-    ]
-  }, integrations: [tailwind(), mdx(), sitemap()]
-}
-)
+    site: 'https://adithyanair.com',
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    markdown: {
+        remarkPlugins: ['remark-gfm', 'remark-smartypants', 'remark-math'],
+        rehypePlugins: ['rehype-mathjax'],
+    },
+    integrations: [mdx(), sitemap()],
+})
