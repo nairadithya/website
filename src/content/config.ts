98ded2f68cbe321d95d633a623bb8843577ea6ea
Adithya Nair <adithyanair121@gmail.com>
2025-03-24 23:02:32 +0530

    Package upgrade

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,24 +1,15 @@
-
 import { z, defineCollection } from 'astro:content'
-const essayCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.date(),
-  }),
-})
 
-const journalCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.date(),
-  }),
+const postCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        date: z.date(),
+        isEssay: z.boolean(),
+    }),
 })
 
 export const collections = {
-  essays: essayCollection,
-  journal: journalCollection
+    essays: postCollection,
 }
