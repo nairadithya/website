e4625ffccae9101340146a49ef70a5fbb8c3bf9f
Adithya Nair <adithyanair121@gmail.com>
2025-04-15 22:16:21 +0530

    Sddmall refactor

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,29 +1,36 @@
 import { z, defineCollection } from 'astro:content'
 import { glob, file } from 'astro/loaders'
 
+blogSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    isEssay: z.boolean().optional(),
+    isDraft: z.boolean().optional(),
+})
+
 const postCollection = defineCollection({
-    loader: glob({ pattern: ['**/*.md', '*.md'], base: './src/content/blog' }),
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        date: z.date(),
-        isEssay: z.boolean(),
-        isDraft: z.boolean().optional(),
-    }),
+    loader: glob({ pattern: ['**/*.md', '*.md'], base: 'src/content/blog' }),
+    schema: blogSchema,
+})
+
+const journeyCollection = defineCollection({
+    loader: glob({ pattern: '*.md', base: 'src/content/journeys' }),
+    schema: blogSchema,
 })
 
 const quotesCollection = defineCollection({
-    loader: file('src/content/notes.json'),
+    loader: glob({ pattern: '*.md', base: 'src/content/quotes/' }),
     schema: z.object({
-        id: z.int(),
         title: z.string(),
-        quote: z.string(),
         author: z.string(),
+        source: z.string(),
         category: z.string(),
     }),
 })
 
 export const collections = {
     blog: postCollection,
+    journeys: journeyCollection,
     quotes: quotesCollection,
 }
