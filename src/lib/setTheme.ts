340a088fe428fab421bf102fa3e3a36edba0e5bc
Adithya Nair <adithyanair121@gmail.com>
2025-06-04 02:34:40 +0530

    New theme setter to not blind the user

---
+++ src/lib/setTheme.ts
@@ -0,0 +1,19 @@
+type theme = 'dark' | 'light'
+
+const getInitialTheme = () => {
+    const savedTheme = localStorage.getItem('theme')
+    if (savedTheme) return savedTheme
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light'
+}
+const applyTheme = (theme: theme) => {
+    if (theme === 'dark') {
+        document.documentElement.classList.add('dark')
+    } else {
+        document.documentElement.classList.remove('dark')
+    }
+    localStorage.setItem('theme', theme)
+}
+
+applyTheme(getInitialTheme())
