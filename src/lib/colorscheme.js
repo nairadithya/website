f68863389eec1245a86894674c1fe1f5ab9edb56
Adithya Nair <adithyanair121@gmail.com>
2025-06-12 02:02:19 +0530

    Updates to hue

--- src/lib/colorscheme.js
+++ src/lib/colorscheme.js
@@ -1,5 +1,24 @@
 // Stolen from https://supergeekery.com/blog/create-mathematically-generated-css-color-schemes-with-oklch
 
+function seededRandom(daySeed) {
+    const x = Math.sin(daySeed) * 10000
+    return x - Math.floor(x)
+}
+
+function getDailyHueSeeded() {
+    const now = new Date()
+    const startOfYear = new Date(now.getFullYear(), 0, 1)
+
+    const dayOfYear = Math.floor(
+        (now.getTime() - startOfYear.getTime()) / (24 * 60 * 60 * 1000)
+    )
+    const randomValue = seededRandom(dayOfYear)
+
+    return Math.floor(randomValue * 360)
+}
+
+const hueSeeded = getDailyHueSeeded()
+document.documentElement.style.setProperty('--hue', `${hueSeeded}deg`)
 /**
  * Creates a pseudo-random float in the range [0, 1) using Math.sin
  * of a seed, ensuring a stable output for each unique integer seed.
@@ -8,30 +27,3 @@ document.addEventListener('astro:page-load', () => {
     const hueSeeded = getDailyHueSeeded()
     document.documentElement.style.setProperty('--hue', `${hueSeeded}deg`)
 })
-
-function seededRandom(daySeed) {
-    const x = Math.sin(daySeed) * 10000
-    return x - Math.floor(x)
-}
-
-/**
- * Returns a pseudo-random hue for the current day, but remains the same
- * for that day. The hue resets the next day.
- */
-function getDailyHueSeeded() {
-    const now = new Date()
-    const startOfYear = new Date(now.getFullYear(), 0, 1)
-
-    // Number of days since Jan 1
-    const dayOfYear = Math.floor(
-        (now.getTime() - startOfYear.getTime()) / (24 * 60 * 60 * 1000)
-    )
-    // Convert that to a random float
-    const randomValue = seededRandom(dayOfYear)
-
-    // Scale up to 360 deg
-    return Math.floor(randomValue * 360)
-}
-
-const hueSeeded = getDailyHueSeeded()
-document.documentElement.style.setProperty('--hue', `${hueSeeded}deg`)
